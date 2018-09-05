620c5855532f09c19cb054170df0ee1779441456
shantanu <sbtalk@gmail.com>
2018-09-05 09:28:52 +0530

    day2 spring boot

---
+++ spring-boot-demo1/src/main/java/com/demo/spring/DemoRunner.java
@@ -0,0 +1,27 @@
+package com.demo.spring;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.demo.spring.entity.Emp;
+
+@Component
+public class DemoRunner implements CommandLineRunner {
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Override
+	public void run(String... args) throws Exception {
+	List<Emp> empList=em.createQuery("select e from Emp e").getResultList();
+	
+	empList.stream().forEach(e->System.out.println(e.getEmpId()+" "+e.getName()+" "+e.getSalary()));
+
+	}
+
+}
